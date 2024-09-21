5a2e5f4dcfe0473b217ebdea2b6d2ab07793933b
Atishay-Jain01 <136550577+Atishay-Jain01@users.noreply.github.com>
2024-09-21 19:20:18 +0530

    new

--- Weather-App/script.js
+++ Weather-App/script.js
@@ -1,10 +1,14 @@
-const input = document.getElementById("search-input");
-const description = document.getElementById("description-text");
+const input = document.getElementById("search-input"); //id in class input tag
+const description = document.getElementById("description-text"); 
 const img = document.getElementById("description-img");
 const locBtn = document.getElementById("loc-btn");
-const section = document.querySelector("section");
+const section = document.querySelector("section"); //by container or tag-name
 const form = document.querySelector("form");
-
+//DOM=html code to js object convert (document.id), this js obj is DOM, tree like structure
+//to provide functionality
+//1. getelmentbyid - method of js, fetch any element, assigned using html-id, returns single obj
+//2. (by classname-array like obj, returns multiple items)
+//3. (by tag-name)
 function getData(e) {
   e.preventDefault();
 
@@ -15,10 +19,15 @@ function getData(e) {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=ca695dcbc66c5fa3d0cb955033fd918f`
     )
-      .then((res) => res.json())
-      .then((data) => {
+      //promise ke 2 states
+      //1.fulfill - (i)then - output of promise (ii)catch - error handle
+      //2.reject
+      //agar resolve ho gya ho toh json format mey convert kr do
+      .then((res) => res.json()) //res(resolve) comes under promise(any thing prarallel execute)
+      .then((data) => { //promise ke andar paas hota h callback fn
+        //why .then? to resolve promise
         // console.log(data);
-        displayWeather(data);
+        displayWeather(data); //if resolve then display weather(everyhting in background)
         document.getElementById("city").style.display = "block";
       });
   }
@@ -43,14 +52,15 @@ function getLocationData() {
     });
   }
 }
-
+//load-method of event listener
+//backend activity catch by eventlistener like hover double click, click
 addEventListener("load", () => {
   const date = document.getElementById("date");
 
   const d = new Date();
   let currentDate = d.toString().slice(4, 15);
 
-  date.innerHTML = currentDate;
+  date.innerHTML = currentDate; //innerHTML - existing content change krne ka tareeka
 });
 
 function displayWeather(data) {
